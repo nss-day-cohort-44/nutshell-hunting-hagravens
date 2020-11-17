ea3edbdfbd675b1534912924af2cd9256739d02c
Shawn Conerton <conerton52@gmail.com>
2020-11-17 15:46:20 -0600

    push! so I can keep it

---
+++ src/scripts/messages/MessageProvider.js
@@ -0,0 +1,21 @@
+const eventHub = document.querySelector(".container")
+
+let message = []
+
+export const getMessage = () => {
+    return fetch('http://localhost:8088/notes', {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(parsedMessages => {
+            notes = parsedNotes
+        })
+}
+
+export const useMessage = () => {
+    return message.slice()
+}
+
+export const sendMessage = (chat) => {
+    
+}
