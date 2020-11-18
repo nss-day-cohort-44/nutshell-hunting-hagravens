9cee0a9ac7a2a2016a8d36d0ed259b14345e090a
TrentSisson <trent.c.sisson0@gmail.com>
2020-11-17 23:20:29 -0600

    finished task provider module function

---
+++ src/scripts/Task/taskList.js
@@ -0,0 +1,14 @@
+import { getTask, useTask } from "./taskProvider.js"
+
+export const taskList = () => {
+getTask()
+.then(() => {
+    const taskArray = useTask()
+    render(taskArray)
+})
+}
+
+const render = () => {
+    letTaskHTMLRepresentations = ""
+    for (const task of taskArray)
+}
--- src/scripts/Task/taskProvider.js
+++ src/scripts/Task/taskProvider.js
@@ -5,14 +5,16 @@
 
 let task = []
 
-export const getTask = () => {
-    return fetch(`http://localhost:8088/tasks`)
-    .then(response => response.json())
-    .then(parsedTask =>
-        task = parsedTask)
-}
-
 export const useTask = () => {
     return task.slice()
 }
-console.log(task)
+
+export const getTask = () => {
+    return fetch(`http://localhost:8088/tasks`)
+    .then(response => response.json())
+    .then(parsedTask => {
+        task = parsedTask
+        console.table(task)
+
+    })
+}
