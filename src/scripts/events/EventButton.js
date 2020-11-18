712a2684fed69589df671e56fc8d2cd034d57ac8
Jaron-Lane <jaronwohlwend@icloud.com>
2020-11-18 15:49:41 -0600

    Commenting update for feature

--- src/scripts/events/EventButton.js
+++ src/scripts/events/EventButton.js
@@ -1,8 +1,9 @@
-const contentTarget = document.querySelector(".addEventButton")
+const eventContentTarget = document.querySelector(".addEventButton")
 const eventHub = document.querySelector(".container")
 
 export const renderNewEventButton = () => {
-    contentTarget.innerHTML = `
+    eventContentTarget.innerHTML = `
     <button id="add-new-event-button">Add New Event</button>
     `
+    // DISPLAYS BUTTON TO DOM
 } 
