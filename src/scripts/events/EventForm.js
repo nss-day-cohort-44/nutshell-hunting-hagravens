bfa6f905db545f6f705ee058b74bf5d8b0f6931f
Jaron-Lane <jaronwohlwend@icloud.com>
2020-11-19 16:26:14 -0600

    pausing for git merge master

--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -2,13 +2,17 @@
 
 // PURPOSE: CREATE THE FORM FOR ADDING A NEW EVENT, AND DISPATCH EVENTS TO THE EVENT PROVIDER
 import { addEvent, useEvents } from "./EventDataProvider.js"
-import { renderNewEventButton } from "./EventButton.js"
 
 const eventHub = document.querySelector(".container")
+const eventTarget = document.querySelector(".eventForm")
 
+eventHub.addEventListener("newEventButtonClicked", () => {
+    console.log("Im listening!")
+    EventForm()
+})
 
 eventHub.addEventListener("click", clickEvent => {
-    if (clickEvent.target.id === "newEventButtonClicked") {
+    if (clickEvent.target.id === "createEventForm__addEventButton") {
         // GRABS INPUT VALUES
         const nameOfEvent = document.querySelector("#createEventForm__eventName").value
         const dateOfEvent = document.querySelector("#createEventForm__eventDat").value
@@ -28,13 +32,11 @@ eventHub.addEventListener("click", clickEvent => {
 
 
 export const EventForm = () => {
-    const eventTarget = document.querySelector("#createEventForm")
     
-    let htmlRep = `<h2>Add New Event</h2>
+    eventTarget.innerHTML = `<h2>Add New Event</h2>
     <input type="text" id="createEventForm__eventName" placeholder="Event Name"/>
     <input type="date" id="createEventForm__eventDate"/>
     <input type="text" id="createEventForm__eventLocation" placeholder="Event Location"/>
     <button id="createEventForm__addEventButton">Add New Event</button>
-    `;
-    eventTarget.innerHTML = htmlRep;
+    `
 }
