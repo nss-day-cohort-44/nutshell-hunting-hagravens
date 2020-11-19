ff292aaa9d0658056bb0f4a180517f4c6e73928e
Jaron-Lane <jaronwohlwend@icloud.com>
2020-11-19 11:29:38 -0600

    Working through htmlRepresentation

--- src/scripts/events/EventForm.js
+++ src/scripts/events/EventForm.js
@@ -1,3 +1,25 @@
-const EventForm = () => {
+// AUTHOR: JARON LANE
+
+// PURPOSE: CREATE THE FORM FOR ADDING A NEW EVENT, AND DISPATCH EVENTS TO THE EVENT PROVIDER
+
+const eventHub = document.querySelector(".container")
+
+
+eventHub.addEventListener("newEventButtonClicked", () => {
     
+})
+
+
+export const EventForm = () => {
+    const eventTarget = document.querySelector("#createEventForm")
+
+    let [month, date, year] = new 
+    
+    let htmlRep = `<h2>Add New Event</h2>
+    <input type="date" id="createEventForm__eventDate"/>
+    <input type="text" id="createEventForm__eventName" placeholder="Event Name"/>
+    <input type="text" id="createEventForm__eventLocation" placeholder="Event Location"/>
+    <button id="createEventForm__addEventButton">Add New Event</button>
+    `;
+    eventTarget.innerHTML = htmlRep;
 }
